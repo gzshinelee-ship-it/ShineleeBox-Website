16928780ff6ba392c3892ede78daa1fc3fb2619a
Shengli <shengli@ShenglideMacBook.local>
2026-06-12 13:38:06 +0800

    feat: deploy batch 1688 greeting cards (GC-002 to GC-006) to Greeting Cards category

---
+++ download_gc_batch.sh
@@ -0,0 +1,50 @@
+#!/bin/bash
+IMAGE_DIR="images/products"
+mkdir -p "$IMAGE_DIR"
+
+# GC-002
+DIR="$IMAGE_DIR/GC-002_3d-popup-festive-bear-balloon-card"
+mkdir -p "$DIR"
+curl -L "https://cbu01.alicdn.com/img/ibank/O1CN01KCkfQl1cF6zf0TWQ7_!!934253570-0-cib.jpg" -o "$DIR/image_01.png"
+curl -L "https://cbu01.alicdn.com/img/ibank/O1CN01uXC70C1cF6zdkqmEN_!!934253570-0-cib.jpg" -o "$DIR/image_02.png"
+curl -L "https://cbu01.alicdn.com/img/ibank/O1CN01UeE8e31cF6zdytVLu_!!934253570-0-cib.jpg" -o "$DIR/image_03.png"
+curl -L "https://cbu01.alicdn.com/img/ibank/O1CN013KxwOQ1cF6zdywnJ1_!!934253570-0-cib.jpg" -o "$DIR/image_04.png"
+curl -L "https://cbu01.alicdn.com/img/ibank/O1CN01fY1CYT1cF6zeqSGW3_!!934253570-0-cib.jpg" -o "$DIR/image_05.png"
+
+# GC-003
+DIR="$IMAGE_DIR/GC-003_custom-3d-popup-invitations-admission-letters"
+mkdir -p "$DIR"
+curl -L "https://cbu01.alicdn.com/img/ibank/O1CN01jrMFED1cF74ccM0pz_!!934253570-0-cib.jpg" -o "$DIR/image_01.png"
+curl -L "https://cbu01.alicdn.com/img/ibank/O1CN01tIEPnj1cF74ccMgQj_!!934253570-0-cib.jpg" -o "$DIR/image_02.png"
+curl -L "https://cbu01.alicdn.com/img/ibank/O1CN01TS2z8T1cF74cDLt39_!!934253570-0-cib.jpg" -o "$DIR/image_03.png"
+curl -L "https://cbu01.alicdn.com/img/ibank/O1CN0166n5Rw1cF74ccMTxN_!!934253570-0-cib.jpg" -o "$DIR/image_04.png"
+curl -L "https://cbu01.alicdn.com/img/ibank/O1CN01z8XElJ1cF74btFFTf_!!934253570-0-cib.jpg" -o "$DIR/image_05.png"
+
+# GC-004
+DIR="$IMAGE_DIR/GC-004_3d-popup-music-light-starry-cake-card"
+mkdir -p "$DIR"
+curl -L "https://cbu01.alicdn.com/img/ibank/O1CN019iNybE1cF6qQOMsbX_!!934253570-0-cib.jpg" -o "$DIR/image_01.png"
+curl -L "https://cbu01.alicdn.com/img/ibank/O1CN01rYSnzN1cF6rQ8sQWV_!!934253570-0-cib.jpg" -o "$DIR/image_02.png"
+curl -L "https://cbu01.alicdn.com/img/ibank/O1CN01xrm3AO1cF6wQQBacN_!!934253570-0-cib.jpg" -o "$DIR/image_03.png"
+curl -L "https://cbu01.alicdn.com/img/ibank/O1CN01tkSIbc1cF6wWvyqkb_!!934253570-0-cib.jpg" -o "$DIR/image_04.png"
+curl -L "https://cbu01.alicdn.com/img/ibank/O1CN01BoN2Sm1cF6wWw2L5t_!!934253570-0-cib.jpg" -o "$DIR/image_05.png"
+
+# GC-005
+DIR="$IMAGE_DIR/GC-005_3d-popup-floral-wedding-car-card"
+mkdir -p "$DIR"
+curl -L "https://cbu01.alicdn.com/img/ibank/O1CN01sFEObm1cF71SCZp3c_!!934253570-0-cib.jpg" -o "$DIR/image_01.png"
+curl -L "https://cbu01.alicdn.com/img/ibank/O1CN01uy8P3l1cF71SQJviQ_!!934253570-0-cib.jpg" -o "$DIR/image_02.png"
+curl -L "https://cbu01.alicdn.com/img/ibank/O1CN01S00lvM1cF74BFGrIG_!!934253570-0-cib.jpg" -o "$DIR/image_03.png"
+curl -L "https://cbu01.alicdn.com/img/ibank/O1CN01WC9H3U1cF74BcUsxn_!!934253570-0-cib.jpg" -o "$DIR/image_04.png"
+curl -L "https://cbu01.alicdn.com/img/ibank/O1CN01NuiED61cF71QOSqw7_!!934253570-0-cib.jpg" -o "$DIR/image_05.png"
+
+# GC-006
+DIR="$IMAGE_DIR/GC-006_3d-popup-valentines-photo-frame-card"
+mkdir -p "$DIR"
+curl -L "https://cbu01.alicdn.com/img/ibank/O1CN01kceP9p1Eon4ZnZthS_!!2212837170399-0-cib.jpg" -o "$DIR/image_01.png"
+curl -L "https://cbu01.alicdn.com/img/ibank/O1CN01L6XXzG1Eon4YRVEGy_!!2212837170399-0-cib.jpg" -o "$DIR/image_02.png"
+curl -L "https://cbu01.alicdn.com/img/ibank/O1CN013Z6P8s1Eon4XemRHp_!!2212837170399-0-cib.jpg" -o "$DIR/image_03.png"
+curl -L "https://cbu01.alicdn.com/img/ibank/O1CN01i6ARpq1Eon4YRUDuh_!!2212837170399-0-cib.jpg" -o "$DIR/image_04.png"
+curl -L "https://cbu01.alicdn.com/img/ibank/O1CN01VLckEh1Eon4XrtiCh_!!2212837170399-0-cib.jpg" -o "$DIR/image_05.png"
+
+echo "Batch Greeting Card Images Downloaded!"
